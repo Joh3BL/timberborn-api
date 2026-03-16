f55211218d910a465a7d22342170e54051ccfa1a
Joh3BL <121747493+Joh3BL@users.noreply.github.com>
2026-03-16 18:55:39 +0100

    Fixed importing issues

--- timberborn_api/__init__.py
+++ timberborn_api/__init__.py
@@ -8,6 +8,6 @@ Authors:
 """
 
 # Initialization code for the timberborn_api package
-from .timberborn_api import TimberbornAPI, L, A
+from .timberborn_api import TimberbornAPI
 
-__all__ = ['TimberbornAPI', 'L', 'A']
+__all__ = ['TimberbornAPI']
