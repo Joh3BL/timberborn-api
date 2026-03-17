a960dc16f9f872ee02080d48b06bf7e7d20845ca
Joh3BL <121747493+Joh3BL@users.noreply.github.com>
2026-03-17 14:17:56 +0100

    Updated test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,36 +1,36 @@
 import unittest
 from timberborn_api import TimberbornAPI
-
-api =  TimberbornAPI()
+import requests
 
 class TestTimberbornAPI(unittest.TestCase):
-    # Test basic functions
+    def setUp(self):
+        # Start API object after fake server is up
+        self.api = TimberbornAPI()
+        requests.get("http://localhost:8080/test/reset")  # Reset fake server values, just in case
+
     def test_list_levers(self):
-        levers = api.list_levers()
+        levers = self.api.list_levers()
         self.assertIn("lever 1", levers)
         self.assertTrue(levers["lever 1"].state)
 
     def test_get_lever(self):
-        lever = api.get_lever("lever 2")
+        lever = self.api.get_lever("lever 2")
         self.assertFalse(lever.state)
 
     def test_set_lever(self):
-        api.set_lever("lever 2", True)
-        lever = api.get_lever("lever 2")
-        self.assertTrue(lever.)
+        self.api.set_lever("lever 2", True)
+        lever = self.api.get_lever("lever 2")
+        self.assertTrue(lever.state)
 
     def test_set_color(self):
-        result = api.set_color("lever 1", "#FF0000")
+        result = self.api.set_color("lever 1", "#FF0000")
         self.assertTrue(result)
 
     def test_get_adapter(self):
-        adapter = api.get_adapter("adapter 2")
+        adapter = self.api.get_adapter("adapter 2")
         self.assertFalse(adapter.state)
 
     def test_list_adapters(self):
-        adapters = api.list_adapters()
+        adapters = self.api.list_adapters()
         self.assertIn("adapter 2", adapters)
         self.assertFalse(adapters["adapter 2"].state)
-
-if __name__ == '__main__':
-    unittest.main()
